7121a21b7924411c35a24a2028ae2e2652c64b8b
essam <essamemad16@gmail.com>
2016-05-01 20:17:33 +0200

    Added initial swipe detection process.

---
+++ Hepta/Hepta/Ball.swift
@@ -0,0 +1,14 @@
+//
+//  Ball.swift
+//  Hepta
+//
+//  Created by Essam on 5/1/16.
+//  Copyright © 2016 Hepta Productions. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Ball: SKNode {
+    
+}
--- Hepta/Hepta/GameScene.swift
+++ Hepta/Hepta/GameScene.swift
@@ -11,35 +11,29 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+       self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        flickStartLocation = touches.first?.locationInNode(self)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let start = flickStartLocation {
+            let end = touches.first?.locationInNode(self)
+            let dx = start.x - (end?.x)!
+            let dy = start.y - (end?.y)!
+            print("x =  \(dx)")
+            print("y = \(dy)")
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    
+    //MARK:- Properties
+    
+    private var flickStartLocation:CGPoint?
 }
